e91d6968f1d7e79858565d561f320baa2a4d592d
kae zhou <kaemihara@outlook.com>
2022-12-15 01:43:36 +0800

    add rtd config

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../ganblr-opensource/'))
+#sys.path.insert(0, os.path.abspath('../../ganblr-opensource/'))
 
 
 # -- Project information -----------------------------------------------------
